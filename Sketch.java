124dfc9ba51027bd21e2609576c35ea77d9cf3a2
Ethan2108 <ethan.au23@ycdsbk12.ca>
2022-05-01 17:52:19 -0400

    added mouse variables. took a long time to understand what to do

--- Sketch.java
+++ Sketch.java
@@ -2,6 +2,9 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
+  float circleX;
+  float circleY;
+  float circleDiameter;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -16,15 +19,36 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    circleX = width/2;
+    circleY = height/2;
+    circleDiameter = 50;
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	   
+
+    background(255);
+
+  if (dist(mouseX, mouseY, circleX, circleY) < circleDiameter/2) {
+    //mouse is inside the circle
+
+    if (mousePressed) {
+      //mouse is inside the circle and clicked
+      //color it bright red and move the circle
+      fill(255, 25, 0);
+      circleX = mouseX;
+      circleY = mouseY;
+    } else {
+      //mouse is inside the circle but not clicked
+      //highlight the circle light green but don't move it
+      fill(128, 256, 128);
+    }
+    } else {
+    //mouse is outside the circle, color it black
+    fill(0);
   }
-  
-  // define other methods down here.
-}
+
+  ellipse(circleX, circleY, circleDiameter, circleDiameter);
+}}
